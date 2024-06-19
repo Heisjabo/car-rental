1bc8b710a91f920127a772518392d0d4fd61261d
soleil00 <srukundo01@gmail.com>
2024-06-19 21:14:22 +0200

    nonsesne

---
+++ src/components/common/brands/Brands.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import BarndCard from "../cards/BarndCard";
+import { Grid } from "@mui/material";
+import { carBrands } from "../../../../constants/cars";
+import { Link } from "react-router-dom";
+
+const Brands = () => {
+  return (
+    <div className="bg-white py-5">
+      <div className="max-w-[1440px] w-[90%] mx-auto">
+        <div className="flex items-center justify-between py-3">
+          <h4 className=" cursor-pointer"> Brands</h4>
+          <Link to="/" className=" underline">
+            View All brand
+          </Link>
+        </div>
+        <Grid container spacing={2}>
+          {carBrands.map((car) => (
+            <Grid item xs={6} sm={4} md={2} key={car.id}>
+              <BarndCard brand={car} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
